90224e2ee9bbbe48d7e4974490c039d92b8cfb03
Jony <xie.yanpeng@nets-tech.com>
2020-10-08 19:30:50 +0800

    10/8 新增ts的config文件

---
+++ typescript/jspang/demoConfig/src/demo2.ts
@@ -0,0 +1,29 @@
+export {}
+class Lady{
+    content = 'hi,帅哥'
+    sayHello(){
+        return this.content
+    }
+}
+
+class xiaojiejie extends Lady{
+    // 类的方法的重写
+    sayHello(){
+        return 'hi,honey'
+    }// 输出hi,honey
+    
+    // 如果注释掉，输出 hi,帅哥
+    
+    sayHello2(){
+        return super.sayHello()+'. 你好！'
+    }
+
+    sayLove(){
+        return 'I love you'
+    }
+}
+
+const lili = new xiaojiejie()
+console.log("lili.sayHello()",lili.sayHello());
+console.log("lili.sayHello2()",lili.sayHello2());
+console.log("lili.sayLove()",lili.sayLove());
