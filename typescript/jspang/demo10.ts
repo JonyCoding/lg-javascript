40b1f1c5c84d44d9f9ded5d50e45f53cdeaff3c3
1070474647@qq.com <1070474647@qq.com>
2020-10-09 20:31:32 +0800

    合并分支

---
+++ typescript/jspang/demo10.ts
@@ -0,0 +1,19 @@
+class Lady{
+    content = "hi,帅哥";
+    sayHello(){
+        return this.content
+    }
+}
+
+// 继承了父类所有的方法
+class xiaojiejie extends Lady{
+    sayLove(){
+        return "I love you"
+    }
+}
+
+
+const goddess = new Lady()
+const goddess2 = new xiaojiejie()
+console.log(goddess2.sayHello());
+console.log(goddess2.sayLove());
---
+++ typescript/webclass/src/07-Enum-types.ts
@@ -0,0 +1,9 @@
+// 枚举类型
+
+export{} // 确保和其他成员没有冲突
+
+const post = {
+    title : 'Hello TypeScript',
+    content: 'TypeScript is a typed superset of javascript.',
+    status: 3  //1  //0
+}
