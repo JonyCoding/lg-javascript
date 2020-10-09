db8500e968e2afc5e10c0004979733ad7a735d6a
Jony <xie.yanpeng@nets-tech.com>
2020-10-09 18:07:40 +0800

    10/9 断言

--- typescript/jspang/demoConfig/src/demo1.ts
+++ typescript/jspang/demoConfig/src/demo1.ts
@@ -3,8 +3,34 @@
 # Author: xie yanpeng
 # Date: 2020-10-08 18:31:49
 # LastEdit: enter your name
-# LastEditTime: 2020-10-08 18:32:08
-# Description: 
+# LastEditTime: 2020-10-09 17:51:43
+# Description: 类型保护
 #
 ============================================================================= */
-const person :string = 'jspang'
+interface Waiter {
+    anjiao: boolean;
+    say: () => {
+
+    }
+}
+interface Teacher {
+    anjiao: boolean;
+    skill: () => {
+
+    }
+}
+// 类型断言
+function judgeWho(animal: Waiter | Teacher) {
+    if (animal.anjiao) {
+        (animal as Teacher).skill()
+    } else {
+        (animal as Waiter).say()
+    }
+}
+function judgeWhoTwo(animal:Waiter|Teacher){
+    if('skill' in animal){
+        animal.skill()
+    }else{
+        animal.say()
+    }
+}
