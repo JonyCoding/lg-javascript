3b17328ed107656a117878752bf1b8bf34a6ac68
Jony <xie.yanpeng@nets-tech.com>
2021-03-03 09:15:11 +0800

    文件夹变更

--- generator/generator-sample/generator/app/index.js
+++ generator/generator-sample/generator/app/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: xie yanpeng
+ * @Date: 2020-12-15 09:12:34
+ * @LastEdit: enter your name
+ * @LastEditTime: 2020-12-16 09:01:42
+ * @Description: 
+ */
 // 此文件为Generator 的核心入口
 //需要导出一个继承自yeoman generator的类型
 //Yeoman Generator 工作时会自动导出我们在此类型中定义的周期方法
@@ -5,6 +12,19 @@
 const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
+    prompting(){
+        // 询问用户环节会调用此方法
+        // 此方法中发出对用户的命令行询问
+
+        return this.prompt([
+            {
+                type:"input",//此项为用户输入
+                name:"name",//键
+                message:"your project name",//命令行询问
+                default:this.appname //当前目录名
+            }
+        ])
+    }
     writing(){
         // Yeoman 自动在生成文件阶段调用此方法
         // 我们可以尝试在这里向目录中写入文件
