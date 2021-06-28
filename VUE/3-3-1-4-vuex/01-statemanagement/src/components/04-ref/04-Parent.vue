96e71270d44900288e88ea4af7216d61469f28eb
Jony <xie.yanpeng@nets-tech.com>
2021-06-28 17:32:04 +0800

    vue X

---
+++ VUE/3-3-1-4-vuex/01-statemanagement/src/components/04-ref/04-Parent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>ref Parent</h1>
+
+    <child ref="c"></child>
+  </div>
+</template>
+
+<script>
+import child from './04-Child'
+export default {
+  components: {
+    child
+  },
+  mounted () {
+    // 此时组件渲染完毕
+    this.$refs.c.focus()
+    this.$refs.c.value = 'hello input'
+  }
+}
+</script>
+
+<style>
+
+</style>
